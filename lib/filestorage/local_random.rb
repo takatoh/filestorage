b060ac4e459baa8f6b1d03d0e8da008bcd760786
takatoh <takatoh.m@gmail.com>
2023-02-10 05:14:04 +0900

    lib/filestorage/local_random.rb(LocalRandom#new): Add keyword argument 'sensitive'.

--- lib/filestorage/local_random.rb
+++ lib/filestorage/local_random.rb
@@ -13,17 +13,26 @@ module Filestorage
     DEFAULT_LETTERS = "abcdefghijklmnopqrstuvwxyz0123456789"
     DEFAULT_LENGTH = 8
 
-    def initialize(base_dir, length: DEFAULT_LENGTH)
+    def initialize(base_dir, length: DEFAULT_LENGTH, sensitive: false)
       @base_dir = Pathname.new(base_dir)
       @filename_length = length
       @letter_pool = DEFAULT_LETTERS
       @pool_size = @letter_pool.size
+      @case_sensitive = sensitive
     end
 
     def store(file)
       begin
         filename = gen_random + File.extname(file)
-        path = [filename[0, 2].upcase, filename[2, 2].upcase, filename].join("/")
+        d1 = filename[0, 2].upcase
+        d2 = filename[2, 2].upcase
+        path = Pathname.new(d1) / d2
+        if @case_sensitive && path.exist?
+          if path.children.map{|f| f.basename.to_s.upcase }.include?(filename.upcase)
+            raise AlreadyExist.new("Already exist #{path / filename}")
+          end
+        end
+        path = path / filename
         super(file, path)
       rescue AlreadyExist
         retry
